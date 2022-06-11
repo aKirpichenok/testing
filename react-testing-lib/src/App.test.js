5ad38ad9789cb073db92459cfa6b370f93482c5d
aKirpichenok <a.kirpichenok99@gmail.com>
2022-06-11 16:03:55 +0300

    first simple test in react

--- react-testing-lib/src/App.test.js
+++ react-testing-lib/src/App.test.js
@@ -3,6 +3,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const helloWordElem = screen.getByText(/hello world/i);
+  const btn = screen.getByRole('button');
+  const input = screen.getByPlaceholderText(/input value/i)
+  expect(helloWordElem).toBeInTheDocument()
+  expect(btn).toBeInTheDocument()
+  // expect(input).toBeInTheDocument()
+  expect(input).toMatchSnapshot()
+  // screen.debug()
 });
