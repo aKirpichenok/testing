6a672bbce45d2302e88aeb191124f1214edb2679
aKirpichenok <a.kirpichenok99@gmail.com>
2022-06-11 16:13:50 +0300

    test find in react-test and other methods

--- react-testing-lib/src/App.js
+++ react-testing-lib/src/App.js
@@ -1,6 +1,16 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
+  const [data, setData] = useState(null)
+  useEffect(() => {
+    setTimeout(() => {
+      setData({})
+    }, 100)
+  }, [])
+
   return (
     <div>
+      {data && <div style={{ color: 'red' }}>data</div>}
       <h1>Hello world!</h1>
       <button>Click me</button>
       <input type="text" placeholder="input value..." />
--- react-testing-lib/src/App.test.js
+++ react-testing-lib/src/App.test.js
@@ -1,14 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const helloWordElem = screen.getByText(/hello world/i);
-  const btn = screen.getByRole('button');
-  const input = screen.getByPlaceholderText(/input value/i)
-  expect(helloWordElem).toBeInTheDocument()
-  expect(btn).toBeInTheDocument()
-  // expect(input).toBeInTheDocument()
-  expect(input).toMatchSnapshot()
-  // screen.debug()
-});
+describe('TEST APP', () => {
+  test('renders learn react link', () => {
+    render(<App />);
+    const helloWordElem = screen.getByText(/hello world/i);
+    const btn = screen.getByRole('button');
+    const input = screen.getByPlaceholderText(/input value/i)
+    expect(helloWordElem).toBeInTheDocument()
+    expect(btn).toBeInTheDocument()
+    // expect(input).toBeInTheDocument()
+    // expect(input).toMatchSnapshot()
+    // screen.debug()
+  });
+
+  test('renders learn react link', async () => {
+    render(<App />);
+    // const helloWordElem = screen.queryByText(/hello2/i)
+    // expect(helloWordElem).toBeNull()
+    screen.debug()
+    const helloWordElem = await screen.findByText(/data/i)
+    expect(helloWordElem).toBeInTheDocument()
+    expect(helloWordElem).toHaveStyle({ color: 'red' })
+    screen.debug()
+  });
+})
+
