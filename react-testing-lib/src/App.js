c88d06c7fb4136de182709f30e4ae4ac41a61daa
aKirpichenok <a.kirpichenok99@gmail.com>
2022-06-11 15:56:21 +0300

    initialize react-test-lib

---
+++ react-testing-lib/src/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <button>Click me</button>
+      <input type="text" placeholder="input value..." />
+    </div>
+  )
+}
+
+export default App;
